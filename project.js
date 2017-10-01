4f27f30c6f8636df1acb671370e79d24123dfd17
menezesmalu <mlmv@cin.ufpe.br>
2017-10-01 15:14:06 -0300

    modularizing

--- project.js
+++ project.js
@@ -10,11 +10,21 @@ var pointsX = [];
 var	pointsY = [];
 var points = [];
 var circleRadius = 5;
+var circles = [];
 
 
 function resizeCanvas(width, height) {
     canvas.width = width;
     canvas.height = height;
+}
+
+function drawLine(x, y){
+	ctx.beginPath();
+		ctx.strokeStyle = color.lines;
+		ctx.lineWidth = 1;
+		ctx.moveTo(pointsX[pointsX.length -2], pointsY[pointsY.length-2]);
+		ctx.lineTo(pointsX[pointsX.length-1], pointsY[pointsY.length-1]);
+		ctx.stroke();
 }
 
 
@@ -28,22 +38,20 @@ function getPosition(event){
 	pointsY.push(y);
 
 	ctx.beginPath();
-	ctx.arc(x, y, circleRadius, 0, 2*Math.PI, false);
+	var c = ctx.arc(x, y, circleRadius, 0, 2*Math.PI, false);
 	ctx.fillStyle = color.circles;
 	ctx.fill();
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = color.lines;
 	ctx.stroke();
+	circles.push(c);
 
-	if(pointsX.length > 1){
-		ctx.beginPath();
-		ctx.strokeStyle = color.lines;
-		ctx.lineWidth = 1;
-		ctx.moveTo(pointsX[pointsX.length -2], pointsY[pointsY.length-2]);
-		ctx.lineTo(pointsX[pointsX.length-1], pointsY[pointsY.length-1]);
-		ctx.stroke();
+
+	if(circles.length > 1){
+		drawLine(x, y);
 	}
 
 }
 
+canvas.addEventListener('dblclick', deletePoint, false);
 
