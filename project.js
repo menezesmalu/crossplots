016c7d3b36aa29cc3ca9f720ba75a9cd6a1b44de
menezesmalu <mlmv@cin.ufpe.br>
2017-10-01 13:28:49 -0300

    set circles and lines colors and a little bit of click in a point. not really good

--- project.js
+++ project.js
@@ -1,18 +1,38 @@
+var color = {
+	circles: "#7B68EE",
+	lines: "black",
+
+}
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var pointsX = [];
+var	pointsY = [];
+var points = [];
+var circleRadius = 5;
+
+
 function resizeCanvas(width, height) {
     canvas.width = width;
     canvas.height = height;
 }
 
-function resizeToFit() {
-    var width = parseFloat(window.getComputedStyle(canvas).width);
-    var height = parseFloat(window.getComputedStyle(canvas).height);
-    resizeCanvas(width, height);
+
+canvas.addEventListener("mousedown", getPosition, false);
+
+function getPosition(event){
+	var x = event.x;
+	var y = event.y;
+
+	x -= canvas.offsetLeft;
+	y -= canvas.offsetTop;
+
+	ctx.beginPath();
+	ctx.arc(x, y, circleRadius, 0, 2*Math.PI, false);
+	ctx.fillStyle = color.circles;
+	ctx.fill();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = color.lines;
+	ctx.stroke();
 }
 
-
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-
-resizeToFit();
 
