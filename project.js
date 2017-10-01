84908f21bd5a6a277f8137716d71786db5051ce1
menezesmalu <mlmv@cin.ufpe.br>
2017-10-01 10:21:38 -0300

    files with a little bit of canvas

--- project.js
+++ project.js
@@ -1,11 +1,18 @@
-var colors = {
-	background: 'LightGrey',
-	curve: 'SlateBlue',
-
+function resizeCanvas(width, height) {
+    canvas.width = width;
+    canvas.height = height;
 }
-var funciona = "workinkg";
 
-function deCasteljau (){
-	
+function resizeToFit() {
+    var width = parseFloat(window.getComputedStyle(canvas).width);
+    var height = parseFloat(window.getComputedStyle(canvas).height);
+    resizeCanvas(width, height);
 }
+
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+resizeToFit();
 
