f2d3f57d277b3301d1b6acbdba72184148fca35a
menezesmalu <mlmv@cin.ufpe.br>
2017-10-28 21:41:57 -0300

    almost there with those bezier curves

--- project.js
+++ project.js
@@ -1,6 +1,7 @@
 var color = {
 	points: "#7B68EE",
 	lines: "black",
+	linescontrol: "red",
 	invisible: "#D3D3D3",
 	button: "buttonface",
 	button2: "red",
@@ -25,21 +26,25 @@ var rect = [
 var points = [];
 var pointsy = [];
 var pointsx = [];
+var pcontrole = [];
+var pcontroley = [];
+var pcontrolex = [];
 var circleRadius = 5;
 var btnpoints = true;
 var btnlines = true;
+var btnbezier = true;
 var move = -1;
 var width = 500;
 var height = 300;
-
-
+var valor = document.getElementById('input_av');
+var av;
+var avaliar = false;
 function resizeCanvas() {
 	for(i in canvas){
 		canvas[i].width = width;
 		canvas[i].height = height;
 	}
 }
-
 function isInCircle(event){
 	var x = event.x - rect[0].left;
 	var y = event.y - rect[0].top;
@@ -69,6 +74,14 @@ function drawPoint(x, y, i){
 	var pnt = {x: x, y: y};
 	return pnt;
 }
+
+function drawCurve(){
+	for(var i = 1; i < pcontrole.length; i++){
+		drawLine(pcontrole[i-1].x, pcontrole[i-1].y,pcontrole[i].x, pcontrole[i].y, color.linescontrol, 0 );
+		//drawLine(pcontroley[i-1].x, pcontroley[i-1].y,pcontroley[i].x, pcontroley[i].y, color.linescontrol, 1 );
+		//drawLine(pcontrolex[i-1].x, pcontrolex[i-1].y,pcontrolex[i].x, pcontrolex[i].y, color.linescontrol, 2 );
+	}
+}
 canvas[0], addEventListener('mousedown', event =>{
 	var x = event.x - rect[0].left;
 	var y = event.y - rect[0].top;
@@ -80,7 +93,11 @@ canvas[0], addEventListener('mousedown', event =>{
 })
 canvas[0].addEventListener('dblclick', event =>{
 	if(isInCircle != -1){
-		deletePoint(event);
+		var i = isInCircle(event);
+		if(i != -1){
+			points.splice(i, 1);
+			resetScreen();
+		}
 	}
 })
 canvas[0].addEventListener('mousemove', function(event) {
@@ -93,6 +110,7 @@ canvas[0].addEventListener('mousemove', function(event) {
 canvas[0].addEventListener('mouseup', function(event) {
     move = -1;
 });
+
 function getPoint(event){
 	//define the point
 	var x = event.x - rect[0].left;
@@ -104,29 +122,10 @@ function getPoint(event){
 		if(points.length > 1) 
 			drawLine(points[points.length-1].x, points[points.length-1].y, points[points.length-2].x, points[points.length-2].y, color.lines,0);
 		resetScreen();
-		pointsy.splice(0, pointsy.length);
-		pointsx.splice(0, pointsx.length);
-		for(var i in points){
-			var cy = drawPoint((width/points.length)*i, points[i].y, 1);
-			pointsy.push(cy);
-			var cx = drawPoint(points[i].x, (height/points.length)*i, 2);
-			pointsx.push(cx);	
-			if(pointsx.length > 1){
-				drawLine(pointsy[pointsy.length-1].x, pointsy[pointsy.length-1].y, pointsy[pointsy.length-2].x, pointsy[pointsy.length-2].y, color.lines,1);
-				drawLine(pointsx[pointsx.length-1].x, pointsx[pointsx.length-1].y, pointsx[pointsx.length-2].x, pointsx[pointsx.length-2].y, color.lines,2);
-			}
-		}
 	}
+	
 }
 
-function deletePoint(event){
-	var i = isInCircle(event);
-	if(i != -1){
-		//deleting the double clicked point
-		points.splice(i, 1);
-		resetScreen();
-	}
-}
 //mudar isso dps  desenhar
 function btnPoints(){
 	//nao mostrar pontos
@@ -154,7 +153,6 @@ function btnPoints(){
 	}
 	btnpoints = !btnpoints;
 }
-
 //mudar isso dps  desenhar
 function btnLines(){ //nao mostrar linhas
 	var btn = document.getElementById('poligonal');
@@ -171,7 +169,6 @@ function btnLines(){ //nao mostrar linhas
 			}
 		}
 	} else { // mostrar pontos
-		console.log(btnlines);
 		btn.style.backgroundColor = color.button;
 		btn.style.color = color.text;
 		for(var i=1; i< points.length; i++){
@@ -183,11 +180,15 @@ function btnLines(){ //nao mostrar linhas
 	btnlines = !btnlines;
 }
 
+
 function resetScreen(){
 	for(j in canvas)
 	ctx[j].clearRect(0, 0, canvas[j].width, canvas[j].height);
 	pointsy.splice(0,pointsy.length);
 	pointsx.splice(0,pointsx.length);
+	pcontrole.slice(0, pcontrole.length);
+	pcontroley.slice(0, pcontroley.length);
+	pcontrolex.slice(0, pcontrolex.length);
 	for(var i in points){
 		var c = drawPoint(points[i].x, points[i].y,0);
 		var cy = drawPoint((width/points.length)*i, points[i].y, 1);
@@ -200,4 +201,65 @@ function resetScreen(){
 			drawLine(pointsx[pointsx.length-1].x, pointsx[pointsx.length-1].y, pointsx[pointsx.length-2].x, pointsx[pointsx.length-2].y, color.lines,2);
 		}
 	}
+	if(avaliar == true) bezierCurve(av);
+}
+
+function pot(a, b){
+	var potencia = 1;
+	for(var i = 0; i < b; i++)
+		potencia = potencia*a;
+	return potencia;
+}
+function fatorial(n){
+	if(n == 1 || n ==0) return 1;
+	else return n*fatorial(n-1);
+}
+function combinacao(n, i){
+	var a = fatorial(n);
+	var b = fatorial(i);
+	var c = fatorial(n-i);
+	return a/(b*c);
+}
+function bezier(t){
+	var x0 = 0;
+	var y0 = 0;
+	var x1 = 0
+	var y1 = 0;
+	var x2 = 0
+	var y2 = 0;
+	for(var i = 0; i < points.length; i++){
+		var comb = combinacao(points.length-1, i);
+		x0 = x0 + (comb)*pot((1-t), points.length-i-1)*pot(t,i)*points[i].x;
+		y0 = y0 + (comb)*pot((1-t), points.length-i-1)*pot(t,i)*points[i].y;
+
+		//x1 = x1 + (comb)*pot((1-t), pointsy.length-i-1)*pot(t,i)*pointsy[i].x;
+		//y1 = y1 + (comb)*pot((1-t), pointsy.length-i-1)*pot(t,i)*pointsy[i].y;
+
+		//x2 = x2 + (comb)*pot((1-t), pointsx.length-i-1)*pot(t,i)*pointsx[i].x;
+		//y2 = y2 + (comb)*pot((1-t), pointsx.length-i-1)*pot(t,i)*pointsx[i].y;
+	}
+	var b0 = {x: x0, y: y0}
+	var b1 = {x: x1, y:y1};
+	var b2 = {x: x2, y: y2}
+
+	pcontrole.push(b0);	
+	pcontroley.push(b1);	
+	pcontrolex.push(b2);
+	return b0;
+}
+function bezierCurve(av){
+	for(var i = 1; i <= av; i++){
+		var b = bezier(i/av);
+	}
+	for(var i = 1; i < pcontrole.length; i++){
+		drawLine(pcontrole[i-1].x, pcontrole[i-1].y,pcontrole[i].x, pcontrole[i].y, color.linescontrol, 0 );
+		//drawLine(pcontroley[i-1].x, pcontroley[i-1].y,pcontroley[i].x, pcontroley[i].y, color.linescontrol, 1 );
+		//drawLine(pcontrolex[i-1].x, pcontrolex[i-1].y,pcontrolex[i].x, pcontrolex[i].y, color.linescontrol, 2 );
+	}
+}
+
+function avaliarC(){
+	av = parseInt(valor.value);
+	avaliar = true
+	bezierCurve(av);
 }
